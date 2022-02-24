230fd5117a9c4a12011cda68067dd7029b7223ac
abdullahhosenakash <abdullahhosenakash@gmail.com>
2022-02-24 17:55:43 +0600

    food details added

--- js/api.js
+++ js/api.js
@@ -1,1 +1,49 @@
-console.log('Hello')
+const searchMeals = () => {
+    const searchFood = document.getElementById('search-food');
+    const searchText = searchFood.value;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => searchResults(data.meals));
+}
+const searchResults = meals => {
+    meals.forEach(meal => {
+        // console.log(meal);
+        const foodCards = document.getElementById('food-cards');
+        const div = document.createElement('div');
+        div.classList.add('cols');
+        div.innerHTML = `
+        <div onclick="loadMealDetail(${meal.idMeal})" class="card h-100">
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${meal.strMeal}</h5>
+                <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+                </div>
+            </div>
+        `;
+        foodCards.appendChild(div);
+    })
+}
+const loadMealDetail = mealId => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealDetail(data.meals[0]));
+}
+
+const displayMealDetail = meal => {
+    console.log(meal)
+    const foodCards = document.getElementById('meal-detail');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${meal.strMeal}</h5>
+            <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+            <a href="${meal.strYoutube}" class="btn btn-primary" target="blank">Watch tutorial on YouTube</a>
+        </div>
+    `;
+    foodCards.appendChild(div);
+
+}
